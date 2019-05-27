baf2b470bb9ec2c443eeeba9596edb9d10693c55
JamesHU <james780902@gmail.com>
2019-05-27 19:33:50 +0800

    5/27

---
+++ ch07/CH07_tset/CH07_tset/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH07_tset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            Airplane myAirplane = new Airplane();
+            ITarffic car = (ITarffic)myCar;
+            int input;
+            while (true)
+            {
+                Console.Write("請問要駕駛->1.車子  2.飛機  其他.離開：");
+                input = int.Parse(Console.ReadLine());
+                if (input == 1)
+                {
+                        myCar.MySpeedUP(60);     //開車子,r參考指向myCar物件實體
+                        car.MySpeedUP(10);
+                }
+                else if (input == 2)
+                {
+                        myAirplane.MySpeedUP(180); //開飛機,r參考指向myAirplane物件實體
+                }
+                else
+                {
+                        break;
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+
+    // 使用介面
+    interface ITarffic
+    {
+       
+
+        void MySpeedUP(int n);
+    }
+    class Totle
+    {
+        public static int miles{get;set;}
+    }
+    class Car : Totle,ITarffic 
+    {
+       
+        public void MySpeedUP(int n)  // 覆寫介面的SpeedUp方法
+        {
+            miles += n; // 使用類別靜態變數
+            Console.WriteLine("\n 駕駛車子, 加速中, 前進 {0} 公里 .", miles);
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+
+    class Airplane : Totle,ITarffic 
+    {
+        
+        public void MySpeedUP(int n)  // 覆寫介面的SpeedUp方法
+        {
+            miles += n; 
+            Console.WriteLine("\n 駕駛飛機, 加速中, 前進 {0} 公里 .", miles);
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+
+}
---
+++ ch07/ConsolePolymorphism3/test1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePolymorphism3
+{
+
+    // 使用介面
+    interface ITarffic
+    {
+        int miles{get; set;}
+        void MySpeedUP(int n);
+    }
+    // 定義Car車子類別繼承自交通工具類別
+    class Car : ITarffic
+    {
+        static int x = 0;
+        void ITarffic._miles(int n)
+        {
+            return x += n;
+        }
+        void ITarffic.MySpeedUP(int n)  // 覆寫父類別的SpeedUp方法
+        {
+            x = _miles(n);
+            Console.WriteLine("\n 駕駛車子, 加速中, 前進 {0} 公里 .", x);
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+    // 定義Airplane飛機類別繼承自交通工具類別
+    class Airplane : ITarffic
+    {
+
+        void ITarffic._miles(int n)
+        {
+            return Car.x += n;
+        }
+        void ITarffic.MySpeedUP(int n)   // 覆寫父類別的SpeedUp方法
+        {
+            x = _miles(n);
+            Console.WriteLine("\n駕駛飛機, 加速中, 前進 {0} 公里 .", Car.x);
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            Airplane myAirplane = new Airplane();
+            int input;
+            while (true)
+            {
+                Console.Write("請問要駕駛->1.車子  2.飛機  其他.離開：");
+                input = int.Parse(Console.ReadLine());
+                if (input == 1)
+                {
+                    myCar.MySpeedUP(15);     //開車子,r參考指向myCar物件實體
+                }
+                else if (input == 2)
+                {
+                    myAirplane.MySpeedUP(150); //開飛機,r參考指向myAirplane物件實體
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
